d9ac093045c1570a8e71e1330214f099326eeea9
Guilherme Alan Ritter <gui.a.ritter@gmail.com>
2020-01-12 20:29:59 -0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# README #
+
+Just a very simple program to process a text and insert a colon in the middle of occurrences of four consecutive digits.
---
+++ src/main/java/io/github/guiritter/hour_minute_formatter/Main.java
@@ -0,0 +1,41 @@
+package io.github.guiritter.hour_minute_formatter;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author Guilherme Alan Ritter
+ */
+public class Main {
+
+    static {
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        JDialog.setDefaultLookAndFeelDecorated(true);
+    }
+
+    public static void main(String args[]) {
+        JFrame frame = new JFrame("Hour Minute Formatter");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JScrollPane pane = new JScrollPane();
+        frame.add(pane, BorderLayout.CENTER);
+
+        JTextArea area = new JTextArea();
+        pane.setViewportView(area);
+        area.setRows(8);
+
+        JButton button = new JButton("format");
+        frame.add(button, BorderLayout.PAGE_END);
+        button.addActionListener(event -> area.setText(area.getText().replaceAll("([0-9]{2})([0-9]{2})", "$1:$2")));
+
+        frame.setVisible(true);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+    }
+}
